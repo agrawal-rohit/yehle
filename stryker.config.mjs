56dfcd18818bb73d8c3d16dbd525adc3678fcfa1
Rohit Agrawal <29514438+agrawal-rohit@users.noreply.github.com>
2026-02-06 16:40:29 +0530

    chore: removed default stryker init comment

--- stryker.config.mjs
+++ stryker.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 /** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
 const config = {
-	_comment:
-		"This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
 	packageManager: "pnpm",
 	reporters: ["html", "json"],
 	testRunner: "vitest",
